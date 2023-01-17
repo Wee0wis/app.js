f25cca4aef3b31ad19f777ff1f731c938d0b71a3
JoanEduardoT <joane.torresespindola@gmail.com>
2023-01-17 11:13:25 -0600

    cambios y practica contador

---
+++ js/vue.js
@@ -0,0 +1,23 @@
+Vue.createApp({
+    data(){
+        return{
+           contador:0
+        }
+    },
+    methods: {
+        incrementar(){
+         this.contador ++;
+        },
+        decremento(){
+            if(this.contador > 0){
+                this.contador --;
+            }
+            else{
+                alert("ya no puedes bajar");
+            }
+            
+            
+        }
+
+    },
+}).mount('#app');
