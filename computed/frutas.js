14b2ec4d2f93cf342cad3c443ca47d60999d39ee
JoanEduardoT <joane.torresespindola@gmail.com>
2023-01-31 11:31:53 -0600

    frutas

---
+++ computed/frutas.js
@@ -0,0 +1,15 @@
+Vue.createApp({
+    data(){
+        return{
+           selector:""
+        }
+    },
+    computed:{
+        fruta(){
+            if(seletor = "manzana"){
+                return 'https://8e9d5b8b8dcb9208ef3f-01db2a53ae0368d03387780ee86ead55.ssl.cf2.rackcdn.com/0000000041480_CF_hyvee_default_large.jpeg'
+            }
+            
+        }
+    }
+}).mount('#App')
